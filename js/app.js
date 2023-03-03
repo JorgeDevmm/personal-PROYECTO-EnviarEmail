f385ca68ff75d848f1a3776863af334bbd34e7d3
Jorge Monzon <jorge29luis10@gmail.com>
2023-03-02 23:31:47 -0500

    Eventos ideales para valdiar un formulario

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Seleccionar los elementos de la interfaz
   const inputEmail = document.querySelector('#email');
-  const inputAsunto = document;
+  const inputAsunto = document.querySelector('#asunto');
+  const inputMensaje = document.querySelector('#mensaje');
 
-  console.log(inputEmail);
+  // Asignar eventos
+
+  // se ejecuta al abandonar un campo
+  inputEmail.addEventListener('blur', (evento) => {
+    console.log(evento.target.value);
+  });
 });
