59997d72d41a6a30b3059b7d484d894695530f0b
Jorge Monzon <jorge29luis10@gmail.com>
2023-03-02 22:55:47 -0500

    Primeros pasos con el proyecto

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// evento que cargue todo el html
+document.addEventListener('DOMContentLoaded', () => {
+  // Seleccionar los elementos de la interfaz
+  const inputEmail = document.querySelector('#email');
+  const inputAsunto = document;
+
+  console.log(inputEmail);
+});
